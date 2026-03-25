b28150611426867d31817d6304caebe499880fd6
Josue <jr5766236@gmail.com>
2026-03-25 07:43:04 -0600

    version 2 (manejo de instancias)

--- src/main/java/com/mx/ux/Unidad2/SensorHeterogeneo/Main.java
+++ src/main/java/com/mx/ux/Unidad2/SensorHeterogeneo/Main.java
@@ -8,9 +8,19 @@ public class Main {
     public static void main(String[] args) {
         List<SensorIA> percepcionHub = new ArrayList<>();
 
-        percepcionHub.add(new SensorLidar());
-        percepcionHub.add(new SensorVision());
-        percepcionHub.add(new SensorUltrasonido());
+        SensorLidar obj1 = new SensorLidar();
+        SensorLidar obj2 = new SensorLidar();
+        SensorVision obj3 = new SensorVision();
+        SensorVision obj4 = new SensorVision();
+        SensorUltrasonido obj5 = new SensorUltrasonido();
+        SensorUltrasonido obj6 = new SensorUltrasonido();
+
+        percepcionHub.add(obj1);
+        percepcionHub.add(obj2);
+        percepcionHub.add(obj3);
+        percepcionHub.add(obj4);
+        percepcionHub.add(obj5);
+        percepcionHub.add(obj6);
 
         System.out.println("Central de datos");
 
