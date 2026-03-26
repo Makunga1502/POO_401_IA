4e7de9c969ec9aa3f5e82cfe74b16c4a49d7f9c4
Josue <jr5766236@gmail.com>
2026-03-26 08:41:14 -0600

    interfaces

---
+++ src/main/java/com/mx/ux/Unidad2/interfaces/Entrenable.java
@@ -0,0 +1,33 @@
+package com.mx.ux.Unidad2.interfaces;
+
+interface Serializable{
+    public void a();
+}
+
+interface Tokenizable{
+    public void b();
+
+}
+
+public interface Entrenable {
+    public void c();
+}
+
+class modelo implements Serializable, Tokenizable, Entrenable{
+
+    @Override
+    public void c() {
+
+    }
+
+    @Override
+    public void a() {
+
+    }
+
+    @Override
+    public void b() {
+    }
+
+}
+
