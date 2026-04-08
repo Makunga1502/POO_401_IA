5df210e12b8cd913da7e9f0800790ba2f8a8f970
Josue <jr5766236@gmail.com>
2026-04-08 07:34:48 -0600

    SerializableModelo

---
+++ src/main/java/com/mx/ux/Unidad2/interfaces/practica03/SerilizableModeloImpl.java
@@ -0,0 +1,8 @@
+package com.mx.ux.Unidad2.interfaces.practica03;
+
+public class SerilizableModeloImpl implements SerializableModelo    {
+    @Override
+    public void guardarEnDisco(String ruta) {
+
+    }
+}
