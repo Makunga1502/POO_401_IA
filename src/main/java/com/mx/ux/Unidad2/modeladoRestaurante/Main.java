602a89d64edc9fb36d2e0db17dd734d5440dbed0
Josue <jr5766236@gmail.com>
2026-04-14 17:59:35 -0600

    modelado de un restaurante

---
+++ src/main/java/com/mx/ux/Unidad2/modeladoRestaurante/Main.java
@@ -0,0 +1,12 @@
+package com.mx.ux.Unidad2.modeladoRestaurante;
+
+public class Main extends Restaurante{
+    public static void main(String[] args) {Restaurante r1 = new Restaurante();
+        r1.nombre = "La Pasta Loca";
+        r1.abrir();
+
+        Restaurante r2 = new Restaurante();
+        r2.nombre = "Sushi Zen";
+        r2.abrir();
+    }
+}
---
+++ src/main/java/com/mx/ux/Unidad2/modeladoRestaurante/Restaurante.java
@@ -0,0 +1,13 @@
+package com.mx.ux.Unidad2.modeladoRestaurante;
+
+class Restaurante {
+    String nombre;
+    String tipoDeComida;
+    int calificacion;
+
+    void abrir() {
+        System.out.println("El restaurante " + nombre + " ya está abierto.");
+    }
+}
+
+
