b5a3c3a84b20a094b91fbdeac801e32f2f5e86ef
Josue <jr5766236@gmail.com>
2026-04-15 17:50:41 -0600

    operacion matematica

---
+++ src/main/java/com/mx/ux/Unidad2/ejercicios/base/OperacionMatematica.java
@@ -0,0 +1,13 @@
+package com.mx.ux.Unidad2.ejercicios.base;
+
+public class OperacionMatematica {
+    public static void main(String[] args) {
+
+        int producto = 8 * 12;
+        int cociente; //
+
+        cociente = 100 / 5;
+        System.out.println("El resultado de la expresión del producto es: " + producto);
+        System.out.println("El resultado de la expresión del cociente es: " + cociente);
+    }
+}
