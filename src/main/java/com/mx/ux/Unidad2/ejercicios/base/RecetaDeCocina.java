1cea075a696e05d206721483f0e11624e1db680d
Josue <jr5766236@gmail.com>
2026-04-15 18:04:48 -0600

    receta de cocina

---
+++ src/main/java/com/mx/ux/Unidad2/ejercicios/base/RecetaDeCocina.java
@@ -0,0 +1,21 @@
+package com.mx.ux.Unidad2.ejercicios.base;
+
+public class RecetaDeCocina {
+    public static void main(String[] args) {
+
+
+        int ingredientesPreparados = 5;
+        int tiempoDeCocina = 30;
+
+
+        System.out.println("Paso 1: Prepara los ingredientes.");
+        System.out.println("Cantidad de ingredientes listos: " + ingredientesPreparados);
+
+
+        System.out.println("Paso 2: Cocina el platillo.");
+        System.out.println("Tiempo estimado de cocción: " + tiempoDeCocina + " minutos.");
+
+
+        System.out.println("El platillo está listo.");
+    }
+}
